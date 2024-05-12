bf84c28aae175b626efdcb5cc8d4934a699c726e
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-05-12 12:50:01 -0600

    remove rotation

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,15 @@ import View from "ol/View";
 const size = 512;
 const BASE_ITERATIONS = 256;
 
-// default zoom, center and rotation
 let zoom = 2;
 let center = [0, 0];
-let rotation = 0;
 
 if (window.location.hash !== "") {
-  // try to restore center, zoom-level and rotation from the URL
   const hash = window.location.hash.replace("#map=", "");
   const parts = hash.split("/");
   if (parts.length === 4) {
     zoom = parseFloat(parts[0]);
     center = [parseFloat(parts[1]), parseFloat(parts[2])];
-    rotation = parseFloat(parts[3]);
   }
 }
 
@@ -100,7 +96,6 @@ const updatePermalink = function () {
   const state = {
     zoom: view.getZoom(),
     center: view.getCenter(),
-    rotation: view.getRotation(),
   };
   window.history.pushState(state, "map", hash);
 };
@@ -115,6 +110,5 @@ window.addEventListener("popstate", function (event) {
   }
   map.getView().setCenter(event.state.center);
   map.getView().setZoom(event.state.zoom);
-  map.getView().setRotation(event.state.rotation);
   shouldUpdate = false;
 });
