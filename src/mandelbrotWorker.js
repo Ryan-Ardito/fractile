f647d52a102d5b9379c1fab9c7cbfa947a144a10
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-05-11 22:00:35 -0600

    bulb checking

--- src/mandelbrotWorker.js
+++ src/mandelbrotWorker.js
@@ -1,4 +1,4 @@
-const ITERATIONS = 2096;
+const ITERATIONS = 4096;
 
 const calculateMandelbrotSet = (z, x, y, size) => {
   const is_in_cardioid_or_bulb = (x_pos, y_pos) => {
@@ -6,7 +6,7 @@ const calculateMandelbrotSet = (z, x, y, size) => {
     let q = Math.pow(x_pos - 0.25, 2) + y2;
     let in_cardioid = q * (q + (x_pos - 0.25)) < 0.25 * y2;
     let in_bulb = Math.pow(x_pos + 1.0, 2) + y2 < 0.0625;
-    in_cardioid || in_bulb;
+    return in_cardioid || in_bulb;
   };
 
   const data = new Uint8Array(size * size * 4);
@@ -17,24 +17,21 @@ const calculateMandelbrotSet = (z, x, y, size) => {
 
   for (let pixelX = 0; pixelX < size; pixelX++) {
     for (let pixelY = 0; pixelY < size; pixelY++) {
-      if (is_in_cardioid_or_bulb(pixelX, pixelY)) {
-        return 0;
-      }
-
       let zx = 0;
       let zy = 0;
       let cx = offsetX + (pixelX * scale) / size;
       let cy = offsetY + (pixelY * scale) / size;
+
       let i = 0;
-
-      while (zx * zx + zy * zy < 4 && i < ITERATIONS) {
-        let temp = zx * zx - zy * zy + cx;
-        zy = 2 * zx * zy + cy;
-        zx = temp;
-        i++;
+      if (!is_in_cardioid_or_bulb(cx, cy)) {
+        while (zx * zx + zy * zy < 4 && i < ITERATIONS) {
+          let temp = zx * zx - zy * zy + cx;
+          zy = 2 * zx * zy + cy;
+          zx = temp;
+          i++;
+        }
+        i = (i - 1) % (ITERATIONS - 1);
       }
-
-      i = (i - 1) % (ITERATIONS - 1);
 
       const brightness = ((i / 100) * 255) | 0;
       let red = (brightness % 8) * 32;
