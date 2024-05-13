7fec2a6129cd7828d80d9492f62a67b773ac0058
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-05-12 21:30:29 -0600

    refactor

--- src/mandelbrotWorker.ts
+++ src/mandelbrotWorker.ts
@@ -14,24 +14,28 @@ const calculateMandelbrotSet = (
   // hacky black real line fix
   z += PERIODICITY_THRESHOLD;
 
-  const isInCardioidOrBulb = (x_pos: number, y_pos: number): boolean => {
-    let y2 = Math.pow(y_pos, 2);
-    let q = Math.pow(x_pos - 0.25, 2) + y2;
-    let inCardioid = q * (q + (x_pos - 0.25)) < 0.25 * y2;
-    let inBulb = Math.pow(x_pos + 1.0, 2) + y2 < 0.0625;
+  const isInCardioidOrBulb = (x: number, y: number): boolean => {
+    let y2 = y * y;
+    let q = Math.pow(x - 0.25, 2) + y2;
+    let inCardioid = q * (q + (x - 0.25)) < 0.25 * y2;
+    let inBulb = Math.pow(x + 1.0, 2) + y2 < 0.0625;
     return inCardioid || inBulb;
   };
 
-  const escapeTime = (cx: number, cy: number, iterations: number): number => {
+  const escapeTime = (
+    cx: number,
+    cy: number,
+    maxIterations: number
+  ): number => {
     let zx = 0;
     let zy = 0;
     let x2 = 0;
     let y2 = 0;
-    let xCycle = 0;
-    let yCycle = 0;
+    let cycleX = 0;
+    let cycleY = 0;
 
     let i = 0;
-    while (i < iterations) {
+    while (i < maxIterations) {
       for (let s = 0; s < 20; s++) {
         if (x2 + y2 > 4) {
           return i;
@@ -45,19 +49,29 @@ const calculateMandelbrotSet = (
 
         if (i >= CYCLE_DETECTION_DELAY) {
           if (
-            Math.abs(zx - xCycle) < PERIODICITY_THRESHOLD &&
-            Math.abs(zy - yCycle) < PERIODICITY_THRESHOLD
+            Math.abs(zx - cycleX) < PERIODICITY_THRESHOLD &&
+            Math.abs(zy - cycleY) < PERIODICITY_THRESHOLD
           ) {
-            return iterations;
+            return maxIterations;
           }
         }
       }
 
-      xCycle = zx;
-      yCycle = zy;
+      cycleX = zx;
+      cycleY = zy;
     }
 
-    return iterations;
+    return maxIterations;
+  };
+
+  const colorPixel = (normalized: number) => {
+    const value =
+      (normalized / (256 + PALETTE_SCALE * Math.log2(normalized))) * 255;
+    let red = (value % 8) * 32;
+    let green = (value % 16) * 16;
+    let blue = (value % 32) * 8;
+
+    return [red, green, blue];
   };
 
   const scale = Math.pow(2, -z) * 4;
@@ -74,11 +88,9 @@ const calculateMandelbrotSet = (
       const index = (pixelY * size + pixelX) * 4;
       if (!isInCardioidOrBulb(cx, cy)) {
         const escape = escapeTime(cx, cy, iterations);
-        const nomalized = escape % iterations;
-        const value = (nomalized / (256 + PALETTE_SCALE * Math.log2(nomalized))) * 255;
-        let red = (value % 8) * 32;
-        let green = (value % 16) * 16;
-        let blue = (value % 32) * 8;
+
+        const normalized = escape % iterations;
+        const [red, green, blue] = colorPixel(normalized);
 
         data[index] = red;
         data[index + 1] = green;
