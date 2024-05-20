f38de45f310ebc8892d54fbe0850ff861ee5b07d
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-05-20 10:27:05 -0600

    less division on gpu

--- src/colorGL.ts
+++ src/colorGL.ts
@@ -1,5 +1,8 @@
 import { ExpressionValue } from "ol/style/webgl";
 
+const RECIPROCAL_60 = 0.016666666666666666;
+const RECIPROCAL_128 = 0.0078125;
+
 const HUE_SCALE = 360;
 const BASE_CONTRAST = 0.5;
 const ITER_FALLOFF = ["var", "iterFalloff"];
@@ -25,7 +28,7 @@ const unpackFloat = (): ExpressionValue => {
 
   const exponent = [
     "-",
-    ["+", ["*", ["%", byte4, 128], 2], ["floor", ["/", byte3, 128]]],
+    ["+", ["*", ["%", byte4, 128], 2], ["floor", ["*", byte3, RECIPROCAL_128]]],
     127,
   ];
 
@@ -70,18 +73,19 @@ const hslToRgb = (
   lightness: ExpressionValue
 ): ExpressionValue => {
   const adjLightness = ["-", 1, ["abs", ["-", ["*", 2, lightness], 1]]];
+
+  const adjHue = ["*", hue, RECIPROCAL_60];
+  const normalizedHue = ["-", 1, ["abs", ["-", ["%", adjHue, 2], 1]]];
+
   const c = ["*", adjLightness, saturation];
+  const x = ["*", c, normalizedHue];
 
-  const adjHue = ["-", 1, ["abs", ["-", ["%", ["/", hue, 60], 2], 1]]];
-  const x = ["*", c, adjHue];
-
-  const m = ["-", lightness, ["/", c, 2]];
-
-  const hueBand = ["floor", ["/", hue, 60]];
+  const hueBand = ["floor", adjHue];
   const r = ["match", hueBand, 0, c, 1, x, 2, 0, 3, 0, 4, x, c];
   const g = ["match", hueBand, 0, x, 1, c, 2, c, 3, x, 4, 0, 0];
   const b = ["match", hueBand, 0, 0, 1, 0, 2, x, 3, c, 4, c, x];
 
+  const m = ["-", lightness, ["*", c, 0.5]];
   const red = ["round", ["*", ["+", r, m], 255]];
   const green = ["round", ["*", ["+", g, m], 255]];
   const blue = ["round", ["*", ["+", b, m], 255]];
