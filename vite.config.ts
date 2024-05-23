0185d718ae5dbc1cc7cc9321fe8e16a65c7ea8f9
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-05-22 20:44:33 -0600

    set up react

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-export default {
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(async () => ({
+  plugins: [react()],
+
   base: "/fractile/",
   build: {
     sourcemap: true,
   },
-};
+}));
