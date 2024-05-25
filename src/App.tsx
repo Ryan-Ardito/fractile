a0624cfd5116bd4d6737569942d81c30aae42bfb
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-05-25 14:11:21 -0600

    band/hue speed working

--- src/App.tsx
+++ src/App.tsx
@@ -28,11 +28,11 @@ function App() {
     prevFrameTime.current = timestamp;
     const framesPassed = elapsed / frameDuration;
 
-    const bandSpeed = Math.min(
-      1,
-      (1 - controlValuesRef.current.bandHueSpeed) * 2
-    );
-    const bandStep = (Math.PI / 60) * bandSpeed * framesPassed;
+    const bandSpeed = Math.min(1, (1 - controlValuesRef.current.bandHueSpeed) * 2);
+    const bandStep =
+      (Math.PI / 60) *
+      bandSpeed *
+      framesPassed;
     let newBandOffset =
       controlValuesRef.current.bandOffset * Math.PI + bandStep;
     if (newBandOffset > Math.PI) {
@@ -41,7 +41,9 @@ function App() {
 
     const hueSpeed = Math.min(1, controlValuesRef.current.bandHueSpeed * 2);
     const hueStep = hueSpeed * framesPassed;
-    let newHueOffset = controlValuesRef.current.hueOffset - hueStep;
+    let newHueOffset =
+      controlValuesRef.current.hueOffset -
+      hueStep;
     if (controlValuesRef.current.hueOffset < -180) {
       newHueOffset += 360;
     }
