01e3a753ad154697e998cd50e9b8648148b105e7
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-06-01 12:12:15 -0600

    slider opacity fix

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -41,104 +41,117 @@ export const Menu = () => {
         >
           {animateButtonText}
         </button>
-        <label>
-          animation speed: {controlValues.animationSpeed} bpm
-          <input
-            type="range"
-            id="animationSpeed"
-            min="1"
-            max="256"
-            step="1"
-            value={controlValues.animationSpeed}
-            ref={(el) => (inputRefs.current[0] = el)}
-            onKeyDown={(e) => handleKeyDown(e, 0)}
-            onChange={(e) => {
-              updateControlValues({
-                type: "SET_ANIMATION_SPEED",
-                payload: parseFloat(e.target.value),
-              });
-            }}
-          />
-        </label>
-        <label>
-          band/hue speed:{" "}
-          {(Math.min(1, (1 - controlValues.bandHueSpeed) * 2) * 100).toFixed(0)}
-          % / {(Math.min(1, controlValues.bandHueSpeed * 2) * 100).toFixed(0)}%
-          <input
-            type="range"
-            id="bandHueSpeed"
-            min="0"
-            max="1"
-            step="0.005"
-            value={controlValues.bandHueSpeed}
-            list="bandHueMarkers"
-            ref={(el) => (inputRefs.current[1] = el)}
-            onKeyDown={(e) => handleKeyDown(e, 1)}
-            onChange={(e) =>
-              updateControlValues({
-                type: "SET_BAND_HUE_SPEED",
-                payload: parseFloat(e.target.value),
-              })
-            }
-          />
-        </label>
-        <label>
-          palette scale: {controlValues.paletteScale}
-          <input
-            type="range"
-            id="paletteScale"
-            min="1"
-            max="10"
-            step="0.01"
-            value={controlValues.paletteScale}
-            ref={(el) => (inputRefs.current[2] = el)}
-            onKeyDown={(e) => handleKeyDown(e, 2)}
-            onChange={(e) =>
-              updateControlValues({
-                type: "SET_PALETTE_SCALE",
-                payload: parseFloat(e.target.value),
-              })
-            }
-          />
-        </label>
-        <label>
-          band spacing: {controlValues.bandSpacing}
-          <input
-            type="range"
-            id="bandSpacing"
-            min="1"
-            max="10"
-            step="0.005"
-            value={controlValues.bandSpacing}
-            ref={(el) => (inputRefs.current[3] = el)}
-            onKeyDown={(e) => handleKeyDown(e, 3)}
-            onChange={(e) =>
-              updateControlValues({
-                type: "SET_BAND_SPACING",
-                payload: parseFloat(e.target.value),
-              })
-            }
-          />
-        </label>
-        <label>
-          band contrast: {controlValues.bandContrast}
-          <input
-            type="range"
-            id="bandContrast"
-            min="0"
-            max="0.5"
-            step="0.01"
-            value={controlValues.bandContrast}
-            ref={(el) => (inputRefs.current[4] = el)}
-            onKeyDown={(e) => handleKeyDown(e, 4)}
-            onChange={(e) =>
-              updateControlValues({
-                type: "SET_BAND_CONTRAST",
-                payload: parseFloat(e.target.value),
-              })
-            }
-          />
-        </label>
+        <div>
+          <label>
+            animation speed: {controlValues.animationSpeed} bpm
+            <input
+              type="range"
+              id="animationSpeed"
+              min="1"
+              max="256"
+              step="1"
+              value={controlValues.animationSpeed}
+              ref={(el) => (inputRefs.current[0] = el)}
+              onKeyDown={(e) => handleKeyDown(e, 0)}
+              onChange={(e) => {
+                updateControlValues({
+                  type: "SET_ANIMATION_SPEED",
+                  payload: parseFloat(e.target.value),
+                });
+              }}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            band/hue speed:{" "}
+            {(Math.min(1, (1 - controlValues.bandHueSpeed) * 2) * 100).toFixed(
+              0
+            )}{" "}
+            % / {(Math.min(1, controlValues.bandHueSpeed * 2) * 100).toFixed(0)}{" "}
+            %
+            <input
+              type="range"
+              id="bandHueSpeed"
+              min="0"
+              max="1"
+              step="0.005"
+              value={controlValues.bandHueSpeed}
+              list="bandHueMarkers"
+              ref={(el) => (inputRefs.current[1] = el)}
+              onKeyDown={(e) => handleKeyDown(e, 1)}
+              onChange={(e) =>
+                updateControlValues({
+                  type: "SET_BAND_HUE_SPEED",
+                  payload: parseFloat(e.target.value),
+                })
+              }
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            palette scale: {controlValues.paletteScale}
+            <input
+              type="range"
+              id="paletteScale"
+              min="1"
+              max="10"
+              step="0.01"
+              value={controlValues.paletteScale}
+              ref={(el) => (inputRefs.current[2] = el)}
+              onKeyDown={(e) => handleKeyDown(e, 2)}
+              onChange={(e) =>
+                updateControlValues({
+                  type: "SET_PALETTE_SCALE",
+                  payload: parseFloat(e.target.value),
+                })
+              }
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            band spacing: {controlValues.bandSpacing}
+            <input
+              type="range"
+              id="bandSpacing"
+              min="1"
+              max="10"
+              step="0.005"
+              value={controlValues.bandSpacing}
+              ref={(el) => (inputRefs.current[3] = el)}
+              onKeyDown={(e) => handleKeyDown(e, 3)}
+              onChange={(e) =>
+                updateControlValues({
+                  type: "SET_BAND_SPACING",
+                  payload: parseFloat(e.target.value),
+                })
+              }
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            band contrast: {controlValues.bandContrast}
+            <input
+              type="range"
+              id="bandContrast"
+              min="0"
+              max="0.5"
+              step="0.01"
+              value={controlValues.bandContrast}
+              ref={(el) => (inputRefs.current[4] = el)}
+              onKeyDown={(e) => handleKeyDown(e, 4)}
+              onChange={(e) =>
+                updateControlValues({
+                  type: "SET_BAND_CONTRAST",
+                  payload: parseFloat(e.target.value),
+                })
+              }
+            />
+          </label>
+        </div>
         <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
           <label>band offset: {controlValues.bandOffset.toFixed(2)}</label>
           <div
@@ -176,7 +189,7 @@ export const Menu = () => {
             </button>
           </div>
           <input
-            style={{ gridColumn: "span 2" }}
+            style={{ gridColumn: "span 2", opacity: "60%" }}
             type="range"
             id="bandOffset"
             min="-3.14"
@@ -229,7 +242,7 @@ export const Menu = () => {
             </button>
           </div>
           <input
-            style={{ gridColumn: "span 2" }}
+            style={{ gridColumn: "span 2", opacity: "60%" }}
             type="range"
             id="hueOffset"
             min="-180"
@@ -247,46 +260,50 @@ export const Menu = () => {
             }
           />
         </div>
-        <label>
-          saturation: {controlValues.saturation}
-          <input
-            type="range"
-            id="saturation"
-            min="0"
-            max="2"
-            step="0.01"
-            value={controlValues.saturation}
-            list="oneMarker"
-            ref={(el) => (inputRefs.current[7] = el)}
-            onKeyDown={(e) => handleKeyDown(e, 7)}
-            onChange={(e) =>
-              updateControlValues({
-                type: "SET_SATURATION",
-                payload: parseFloat(e.target.value),
-              })
-            }
-          />
-        </label>
-        <label>
-          lightness: {controlValues.lightness}
-          <input
-            type="range"
-            id="lightness"
-            min="0"
-            max="2"
-            step="0.01"
-            value={controlValues.lightness}
-            list="oneMarker"
-            ref={(el) => (inputRefs.current[8] = el)}
-            onKeyDown={(e) => handleKeyDown(e, 8)}
-            onChange={(e) =>
-              updateControlValues({
-                type: "SET_LIGHTNESS",
-                payload: parseFloat(e.target.value),
-              })
-            }
-          />
-        </label>
+        <div>
+          <label>
+            saturation: {controlValues.saturation}
+            <input
+              type="range"
+              id="saturation"
+              min="0"
+              max="2"
+              step="0.01"
+              value={controlValues.saturation}
+              list="oneMarker"
+              ref={(el) => (inputRefs.current[7] = el)}
+              onKeyDown={(e) => handleKeyDown(e, 7)}
+              onChange={(e) =>
+                updateControlValues({
+                  type: "SET_SATURATION",
+                  payload: parseFloat(e.target.value),
+                })
+              }
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            lightness: {controlValues.lightness}
+            <input
+              type="range"
+              id="lightness"
+              min="0"
+              max="2"
+              step="0.01"
+              value={controlValues.lightness}
+              list="oneMarker"
+              ref={(el) => (inputRefs.current[8] = el)}
+              onKeyDown={(e) => handleKeyDown(e, 8)}
+              onChange={(e) =>
+                updateControlValues({
+                  type: "SET_LIGHTNESS",
+                  payload: parseFloat(e.target.value),
+                })
+              }
+            />
+          </label>
+        </div>
         <datalist id="zeroMarker">
           <option value="0"></option>
         </datalist>
